e16c2e9f181b095529d5b1a469deb50bce4e0993
mzkki <08harismu@gmail.com>
2024-12-05 12:47:25 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const router = require('./routes');
+const { notFoundHandler, errorHandler } = require('./utils/responseHandler');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World'
+  });
+})
+
+app.use('/api', router);
+
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+})
